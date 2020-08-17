18dfce684b9ac761b9998e430946d824d21e5d11
WebKawsar <web.kawsarahmed@gmail.com>
2020-08-17 20:00:50 +0600

    assignment condition successfull

---
+++ scripts.js
@@ -0,0 +1,84 @@
+document.getElementById("search").addEventListener("click", function(){
+
+    const songName = document.getElementById("song-name").value;
+
+    const api = "https://api.lyrics.ovh/suggest/";
+    const url = `${api}${songName}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+
+        const results = document.getElementById("results");
+        const getTitle = data.data[0].title;
+        const getArtist = data.data[0].artist.name;
+    
+        for (let i = 0; i < 10; i++) {
+            const element = data.data[i];
+            const title = element.title;
+            const artist = element.artist.name;
+
+            results.innerHTML +=    `<p class="author lead">
+                                        <strong id="title-name">${title}</strong> - Album by <span id="artist-name">${artist}</span>
+                                        <button  class="btn btn-success show-lyrics">Get Lyrics</button>
+                                    </p>`;
+
+        }
+        
+        const buttons = document.getElementsByClassName("show-lyrics");
+        
+        for (let i = 0; i < buttons.length; i++) {
+            const element = buttons[i];
+
+            element.addEventListener("click", function(event){
+
+                const getThisTitle = this.parentNode.childNodes[1].innerHTML;
+                const getThisArtist = this.parentNode.childNodes[3].innerHTML;
+
+                const secondUrl = `https://api.lyrics.ovh/v1/${getThisArtist}/${getThisTitle}`;
+
+                fetch(secondUrl)
+                .then(secondRes => secondRes.json())
+                .then(secondData => {
+
+                    const lyrics = secondData.lyrics;
+                    if(lyrics == undefined){
+                        
+                        const setLyrics = document.getElementById("single-lyrics");
+                        setLyrics.innerHTML = `
+                                            <h2 class="text-success mb-4">${getThisTitle}- ${getThisArtist}</h2>
+                                            <pre class="lyric text-white">This Song not found. Try others song !!!!.....</pre>`;
+
+                    }
+                    else {
+
+                        const setLyrics = document.getElementById("single-lyrics");
+                        setLyrics.innerHTML = `
+                                            <h2 class="text-success mb-4">${getThisTitle}- ${getThisArtist}</h2>
+                                            <pre class="lyric text-white">${lyrics}</pre>`;
+
+                    }
+
+
+                })
+
+            });
+            
+        }
+        
+        
+        // <button class="btn go-back">&lsaquo;</button>
+        // event.preventDefault();
+        document.getElementById("song-name").value = "";
+    
+    })
+
+});
+
+
+
+
+
+
+
+
